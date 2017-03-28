9ad782ca9158468da12127d5d87cef0f5f93ae1e
Samuel Stenton <sthstenton@gmail.com>
2017-03-29 00:18:20 +0100

    Use handlebars for templating

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ var bodyParser  = require('body-parser');
 var DB          = require('./database/db.js');
 var User        = require('./models/user.js');
 var db          = new DB();
+var hbs         = require('express-hbs');
 
 User.where({name: 'James Wallis'}, function(users) {
 	for (var user in users) {
@@ -17,27 +18,33 @@ User.where({name: 'James Wallis'}, function(users) {
 	}
 });
 
-// configure app to use bodyParser()
-// this will let us get the data from a POST
+app.engine('hbs', hbs.express4({
+  partialsDir: __dirname + '/views/partials'
+}));
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
 
 var port = process.env.PORT || 8080;        // set our port
 
 // ROUTES FOR OUR API
 // =============================================================================
+var api = express.Router();              // get an instance of the express Router
 var router = express.Router();              // get an instance of the express Router
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
+api.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
 });
 
-// more routes for our API will happen here
+router.get('/', function(req, res) {
+    res.render('register/index');   
+});
 
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
+app.use('/api', api);
+app.use('/', router);
 
 // START THE SERVER
 // =============================================================================
