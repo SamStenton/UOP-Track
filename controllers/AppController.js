5a25f7154974ee65ae491bb9c3db112fb8bf1f52
Samuel Stenton <sthstenton@gmail.com>
2017-03-29 14:14:59 +0100

    Begin the actual dashboard!

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -3,17 +3,9 @@ var controller = require('./controller.js')
 class AppController extends controller {
 
     index(req, res) {
-        res.render('register/index');   
-    }
+        res.render('dashboard/index');   
+    } 
 
-    about(req, res) {
-        res.send('about page')
-    }
-
-    post(req, res) {
-        console.log(req)
-        res.json('asdasd')
-    }
 }
 
 module.exports = AppController
