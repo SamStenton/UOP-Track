c1039420b1069da05abb13120f43ea6e7709af28
Samuel Stenton <sthstenton@gmail.com>
2017-03-30 03:33:23 +0100

    Create a modules with submissions

--- controllers/API/ModuleController.js
+++ controllers/API/ModuleController.js
@@ -1,10 +1,26 @@
 var controller = require('../controller.js')
+var Module = require('../../models/module.js')
 
 class ModuleController extends controller {
 
     create(req, res) {
-        console.log(req.body)
-        res.json({message: 'wahhooo'});   
+        // console.log(req.body)
+        Module.create({
+            course_id: 1,
+            name: req.body.name
+        }).then(module => {
+            let submissions = req.body.submissions
+            for(let key in submissions) {
+                module.createItem({
+                    name: submissions[key].name,
+                    weighting: submissions[key].weight,
+                    grade: submissions[key].grade
+                })
+            }
+            res.json({message: 'wahhooo'});   
+        })
+
+        // res.json({message: 'wahhooo'});   
     }
 }
 
